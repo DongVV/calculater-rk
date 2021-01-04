2aedc14c5c6616957e68c44864652386dbd56b20
KhanhDong <dongvu@carro.co>
2021-01-04 10:06:33 +0700

    first commit

---
+++ pages/const.js
@@ -0,0 +1,32 @@
+export const FIELDS = [
+    {
+        lable: 'RK61',
+        name: 'rk_61',
+        value: 510
+    },
+    {
+        lable: 'RK61 RGB',
+        name: 'rk_61_rgb',
+        value: 580
+    },
+    {
+        lable: 'RK71',
+        name: 'rk_71',
+        value: 600
+    },
+    {
+        lable: 'RK71 RGB',
+        name: 'rk_71_rgb',
+        value: 690
+    },
+    {
+        lable: 'RK987',
+        name: 'rk_987',
+        value: 620
+    },
+    {
+        lable: 'RK918',
+        name: 'rk_918',
+        value: 630
+    },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,158 @@
-import Head from 'next/head'
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Form, Row, Col, Input, Button, Card, List } from 'antd';
+import styled from 'styled-components';
+import { FIELDS } from './const';
 
-export default function Home() {
+const WrapperStyle = styled.div`
+  margin: 10px;
+
+  .ant-card {
+    margin-bottom: 15px;
+  }
+`;
+
+const FormItem = Form.Item;
+
+const Home = () => {
+  const [totalRevenueOfM, setTotalRevenueOfM] = useState(0);
+  const [totalOriginOfM, setTotalOriginOfM] = useState(0);
+  const [totalProfitOfM, setTotalProfitOfM] = useState(0);
+
+  const [totalRevenueOfKD, setTotalRevenueOfKD] = useState(0);
+  const [totalOriginOfKD, setTotalOriginOfKD] = useState(0);
+  const [totalProfitOfKD, setTotalProfitOfKD] = useState(0);
+
+  const getFields = (cal = true, flag) => {
+    const children = [];
+
+    let formItemLayout = {
+      labelCol: { span: 9},
+      wrapperCol: { span: 15 },
+    };
+    let colSpan = 4;
+    if (cal) {
+      formItemLayout = {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 18 },
+      }
+      colSpan = 24;
+    }
+
+    FIELDS.map((field, index) => {
+      children.push(
+        <Col span={colSpan} key={index}>
+          <FormItem
+            {...formItemLayout}
+            label={field.lable}
+            name={cal ? `cal_${flag}_${field.name}` : field.name}
+            initialValue={cal ? 0 : field.value}
+          >
+            <Input placeholder={field.lable} />
+          </FormItem>
+          {cal && <FormItem
+            {...formItemLayout}
+            wrapperCol={{ span: 3 }}
+            label={`Qty ${field.lable}`}
+            name={`number_${flag}_${field.name}`}
+            initialValue={0}
+          >
+            <Input placeholder={field.lable} />
+          </FormItem>}
+        </Col>
+      );
+    });
+
+    return children;
+  }
+
+  const handleSubmit = data => {
+    let totalRevenue = 0;
+    let totalOrigin = 0;
+
+    FIELDS.map(field => {
+      totalRevenue += eval(data[`cal_M_${field.name}`]);
+      totalOrigin += ((parseInt(data[field.name]) + data['shipping_fee']) * parseInt(data[`number_M_${field.name}`]));
+    });
+    let totalProfitTmp = totalRevenue - totalOrigin;
+
+    setTotalRevenueOfM(totalRevenue);
+    setTotalOriginOfM(totalOrigin);
+    setTotalProfitOfM(totalProfitTmp);
+
+    totalRevenue = 0;
+    totalOrigin = 0;
+    totalProfitTmp = 0;
+
+    FIELDS.map(field => {
+      totalRevenue += eval(data[`cal_KD_${field.name}`]);
+      totalOrigin += ((parseInt(data[field.name]) + data['shipping_fee']) * parseInt(data[`number_KD_${field.name}`]));
+    });
+    totalProfitTmp = totalRevenue - totalOrigin;
+
+    setTotalRevenueOfKD(totalRevenue);
+    setTotalOriginOfKD(totalOrigin);
+    setTotalProfitOfKD(totalProfitTmp);
+  }
+
+  const getList = (totalRevenue, totalOrigin, totalProfit) => {
+    return <List
+      size="large"
+      bordered
+      dataSource={[
+        `Total Revenue: ${totalRevenue}`,
+        `Total Origin: ${totalOrigin}`,
+        `Total Profit: ${totalProfit}`,
+      ]}
+      renderItem={item => <List.Item>{item}</List.Item>}
+    />
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <WrapperStyle>
+      <Form className="ant-advanced-search-form" onFinish={handleSubmit}>
+        <Card title="The Origin Price Without The Shipping Fee">
+          <Row gutter={40}>
+            {getFields(false)}
+          </Row>
+          <Row gutter={40}>
+            <FormItem
+              label="Shipping Fee"
+              name="shipping_fee"
+              initialValue={20}
+            >
+              <Input placeholder="Shipping Fee" />
+            </FormItem>
+          </Row>
+        </Card>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Card title="Calculate">
+          <Row gutter={40}>
+            <Col span="12">
+              <Card title="Minh">
+                <Row gutter={40}>{getFields(true, "M")}</Row>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                {getList(totalRevenueOfM, totalOriginOfM, totalProfitOfM)}
+              </Card>
+            </Col>
+            <Col span="12">
+              <Card title="KD">
+                <Row gutter={40}>{getFields(true, "KD")}</Row>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                {getList(totalRevenueOfKD, totalOriginOfKD, totalProfitOfKD)}
+              </Card>
+            </Col>
+          </Row>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          {getList(`${totalRevenueOfM + totalRevenueOfKD}`, `${totalOriginOfM + totalOriginOfKD}`, `${totalProfitOfM + totalProfitOfKD}/2 = ${(totalProfitOfM + totalProfitOfKD)/2}`)}
+        </Card>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">Open bold</Button>
+        </Form.Item>
+      </Form>
+    </WrapperStyle>
+  );
+};
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
+export default Home;
